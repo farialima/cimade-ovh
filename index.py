05010800277bea6f30407299f26f684dacb4183a
Francois Granade <github.com@farialima.net>
2021-04-19 06:28:59 -0700

    better presentation, favicon...

--- index.py
+++ index.py
@@ -15,6 +15,7 @@ import subprocess
 from email.message import EmailMessage
 from html import escape
 from functools import lru_cache
+import logging
 
 import pytz
 import ovh
@@ -28,6 +29,13 @@ WEEKDAYS = [
     'saturday',
     'sunday',
     ]
+
+
+logging.basicConfig(filename=join(dirname(abspath(__file__)), "activity.log"),
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
 try:
     # python >= 3.7
@@ -252,52 +260,35 @@ class Queue(Client):
             print(condition_details)
 
 
-def notify(message):
+def notify(message, city='(no city)'):
+    logging.info(f'for {city}: {message}')
+
     msg = EmailMessage()
     script_uri = os.environ.get('SCRIPT_URI', '(non connu)') # SCRIPT_URL is set by mod_rewrite
     msg.set_content(message + "\n\nVoir le site des permanence: " + script_uri)
     msg['From'] = os.environ.get('SERVER_ADMIN', 'admin@inconnu') 
     msg['To'] = "ovh-notification@farialima.net"
-    msg['Subject'] = f"Permtel notification pour {CITY}"
+    msg['Subject'] = f"Permtel notification pour {city}"
     
     subprocess.run(["/usr/sbin/sendmail", "-t", "-oi"], input=msg.as_bytes())
 
 
-CITY = os.environ['REQUEST_URI'].replace('/', '').capitalize() or 'Undefined'
+def do_page(city, line):
 
-
-def do_page():
-
-    print("Content-type: text/html\n\n")
     print_html(f'''<html>
+<head>
+<title>Cimade - configuration des redirections pour {city}</title>
+<link rel="shortcut icon" href="https://permtel.farialima.net/favicon.ico"/>
+</head>
 <body>
 ''')
-
-    try:
-        if CITY == "Lyon":
-            line = Queue('ovhtel-17862213-1', '0033478284789')
-        elif CITY == "Lille":
-            line = Redirect('ovhtel-15669832-1', '0033320543514')
-        else:
-            print_html('''
-  <h1>Permanences T&eacute;l&eacute;phoniques Cimade</h1>
-  <p>Choisissez votre ville :</p>
-    <p><a href="/lyon">Lyon</a></p>
-    <p><a href="/lille">Lille</a></p>
-  </body>
-<html>''')
-            return
-    except Exception as e:
-        notify(str(e))
-        print(e)
-        raise
-
+    
     tel = ''
     now = french_datetime()
-    print_html(f'''<h1>Permanences T&eacute;l&eacute;phoniques Cimade {CITY}</h1>
-<p><b>Cette page est pour {CITY} sur le num&eacute;ro {line.get_tel()}.</b><br/>
+    print_html(f'''<h1>Permanences T&eacute;l&eacute;phoniques Cimade {city}</h1>
+<p><b>Cette page est pour {city} sur le num&eacute;ro {line.get_tel()}.</b><br/>
 <i>Pour la liste des villes, allez <a href="/">ici</a></i></p>
-<p><i>Page actualisée le {now}. <a href="./">Actualiser cette page</a></i></p>
+<p><i>Page actualisée le {now}. <a href="">Actualiser cette page</a></i></p>
 ''')
     
 
@@ -311,7 +302,7 @@ def do_page():
             if 'finish' in multiform:
                 line.stop_perm()
                 print_html(f'<p style="color: blue">Permanence terminée</p>')
-                notify('Permanence terminée')
+                notify('Permanence terminée', city)
             else:
                  # unescape needed because when copy/pasting on Safari, getting chars like "&#8236;" !!
                 tel = html.unescape(multiform['tel'][0]) if 'tel' in multiform else ''
@@ -319,14 +310,14 @@ def do_page():
                 line.set_agent(number)
                 line.start_perm()
                 print_html(f'<p style="color: blue">Permanence commencée sur le numéro {tel}</p>')
-                notify(f'Permanence commencée sur le numéro {tel}')
+                notify(f'Permanence commencée sur le numéro {tel}', city)
            
         except Exception as e:
             print_html(f'''<p style="color: red">Erreur: {e}</p>
 <p>Si probl&egrave;me, contactez François au 06 99 12 47 55</p>
-<p><i><a href="./">Actualiser cette page</a></i></p>
+<p><i><a href="">Actualiser cette page</a></i></p>
 ''')
-            notify(f'error when setting permanence: {e}')
+            notify(f'error when setting permanence: {e}', city)
 
     print('''<h2>&Eacute;tat actuel</h2>''')
     
@@ -373,7 +364,40 @@ Si vous avez fini, cliquez&nbsp;:&nbsp;<input type="submit" value="Terminer la p
 </html>
 ''')
 
-do_page()
+def main():
+    print("Content-type: text/html\n\n")
+    try:
+        city = os.environ['REQUEST_URI'].replace('/', '').capitalize()
+   
+        if city == "Lyon":
+            line = Queue('ovhtel-17862213-1', '0033478284789')
+        elif city == "Lille":
+            line = Redirect('ovhtel-15669832-1', '0033320543514')
+        else:
+            line = None
+    except Exception as e:
+        notify(str(e))
+        print("<html><body>Une erreur s'est produite. Contactez Fran&ccedil;ois au 06 99 12 47 55</body><html>")
+        return
+    
+    if line:
+        do_page(city, line)
+    else:
+        print(f'''<html>
+<head>
+<title>Cimade - configuration des redirections</title>
+<link rel="shortcut icon" href="https://permtel.farialima.net/favicon.ico"/>
+</head>
+<body>
+  <h1>Permanences T&eacute;l&eacute;phoniques Cimade</h1>
+  <p>Choisissez votre ville :</p>
+    <p><a href="/lyon">Lyon</a></p>
+    <p><a href="/lille">Lille</a></p>
+  </body>
+<html>''')
+
+if __name__ == "__main__":
+    main()
 
 
 # for maintenance..
